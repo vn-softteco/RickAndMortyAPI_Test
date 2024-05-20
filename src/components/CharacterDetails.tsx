79bc339b8f3e3cee51a13d1d14ba58909cd4690b
Vladimir Nalivaiko <v.nalivaiko@softteco.com>
2024-05-20 23:00:36 +0300

    Removed bug for characters details

--- src/components/CharacterDetails.tsx
+++ src/components/CharacterDetails.tsx
@@ -1,4 +1,5 @@
 import { Character } from "@/types";
+import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -10,36 +11,37 @@ type CharacterDetailsProps = {
 
 const CharacterDetails = function ({ character }: CharacterDetailsProps) {
     return (
-        <Card
-            sx={{
-                margin: "0 auto",
-                display: "flex"
-            }}
-        >
-            <CardMedia
-                component="img"
-                image={character.image}
-                alt={character.name}
-                width={300}
-                height={300}
-            />
-            <CardContent
+        <Box display="flex">
+            <Card
                 sx={{
-                    display: "flex",
-                    flexDirection: "column"
+                    margin: "0 auto"
                 }}
             >
-                <Typography alignSelf="start" gutterBottom variant="h4">
-                    {character.name}
-                </Typography>
-                <Typography alignSelf="start" component="p" variant="h5">
-                    {character.species}
-                </Typography>
-                <Typography alignSelf="start" component="p">
-                    {character.gender}
-                </Typography>
-            </CardContent>
-        </Card>
+                <CardMedia
+                    component="img"
+                    image={character.image}
+                    alt={character.name}
+                    width={300}
+                    height={300}
+                />
+                <CardContent
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column"
+                    }}
+                >
+                    <Typography alignSelf="start" gutterBottom variant="h4">
+                        {character.name}
+                    </Typography>
+                    <Typography alignSelf="start" component="p" variant="h5">
+                        {character.species}
+                    </Typography>
+                    <Typography alignSelf="start" component="p">
+                        {character.gender}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Box>
     );
 };
 
